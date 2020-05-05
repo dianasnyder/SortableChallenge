56096edd232b98e7909c51e780c0335165dcece9
Diana Snyder <dianajsnyder@gmail.com>
2020-05-05 00:54:04 -0400

    Implement auction logic

--- auction.py
+++ auction.py
@@ -1,29 +1,60 @@
-print("hello world")
+import json
 
-# load config
-
-# dict keyed on site name
-sitesConfig = {}
-# dict keyed on bidder name
-bidderAdjustments = {}
-
-# load input (generator? - probably overkill without proven need)
-
-auctionResults = []
-for auction in auctionList:
-    #verify site
-
-    # make a class that contains these fields with unit name, then make a dict
-    # highestAdjustedBid determines auction winner
+class UnitAuction:
     highestAdjustedBid = 0
     highestBid = None
-    # consider refactor in case json field name changes
+
+# load config
+with open("config.json") as config:
+    configData = json.load(config)
+
+# dict keyed on site name
+sitesConfig = {site["name"]:site for site in configData["sites"]}
+# dict keyed on bidder name
+bidderAdjustments = {bidder["name"]:bidder for bidder in configData["bidders"]}
+
+# load input
+with open("input.json") as input:
+    auctionList = json.load(input)
+
+
+
+# run auction
+auctionResults = []
+for auction in auctionList:
+    auctionResult = []
+    siteData = sitesConfig.get(auction["site"])
+    # only hold auction if valid site
+    if not siteData:
+        auctionResults.append(auctionResult)
+        continue
+
+    unitAuctions = {unit: UnitAuction() for unit in auction["units"]}
+
     for bid in auction["bids"]:
+        print("\n", bid, "\n")
+        unitAuction = unitAuctions.get(bid["unit"])
+        bidderAdjustment = bidderAdjustments[bid["bidder"]]
+
+        adjustedBid = bid["bid"] 
         # can only win auction if valid bidder for site, for a valid unit, bidder is in biddersList
         # and adjusted value >= bid floor
-        if sitesConfig.get(bid.bidder):
+        # linear search through bidders - could make constant by transforming bidders lists to dict
+        if bid["bidder"] in siteData["bidders"] and unitAuction and bidderAdjustment:
+            adjustedBid = bid["bid"] * (1 + bidderAdjustment["adjustment"])
+            if (adjustedBid > siteData["floor"] and adjustedBid > unitAuction.highestAdjustedBid):
+                unitAuction.highestAdjustedBid = adjustedBid
+                unitAuction.highestBid = bid
+                print("\nbid winner found")
+                print(unitAuction.highestAdjustedBid)
+                print(unitAuction.highestBid)
 
-    # add to list of auction results if auction was valid
-
+    # verify output proper if auction invalid or no one wins
+    auctionResults.append([unitAuction.highestBid for unitName, unitAuction in unitAuctions.items()])
 
 print(auctionResults)
+
+#TODO: output auctionResults
+
+
+
